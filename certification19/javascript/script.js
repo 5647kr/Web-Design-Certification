14715e6cc7a23466968aab8cfaa6131a605ed980
5647kr <119591236+5647kr@users.noreply.github.com>
2024-07-23 15:50:57 +0900

    certification19 재풀이

--- certification19/javascript/script.js
+++ certification19/javascript/script.js
@@ -1,29 +1,28 @@
 jQuery(document).ready(function() {
   // 탭메뉴
-  $(".submenu").hide();
   $(".navi>li").mouseover(function() {
     $(this).find(".submenu").stop().slideDown(500)
   }).mouseout(function() {
     $(this).find(".submenu").stop().slideUp(500)
-  })
+  });
 
   // 이미지슬라이드
-  $(".imgslide a:gt(0)").hide();
-  setInterval(() => {
+  $(".imgslide a:gt(0)").hide()
+  setInterval(function() {
     $(".imgslide a:first-child").fadeOut().next("a").fadeIn().end().appendTo(".imgslide")
-  }, 3000);
+  }, 3000)
 
   // 탭기능
-  $(".tab>ul>li>a").click(function() {
-    $(this).parent().addClass("active").siblings().removeClass("active");
+  $(".tab ul li a").click(function() {
+    $(this).parent().addClass("active").siblings().removeClass("active")
     return false;
   })
 
-  // 레이어팝업
-  $(".notice ul li:first-child").click(function() {
+  // 레이어 팝업
+  $(".notice ul li a:first-child").click(function() {
     $("#layer").addClass("active")
   })
   $(".btn").click(function() {
     $("#layer").removeClass("active")
-  })
+  });
 })
