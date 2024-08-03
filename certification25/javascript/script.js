269b6f7e868cf96ffb0c78a585f350c68a4ab62e
5647kr <119591236+5647kr@users.noreply.github.com>
2024-08-03 15:56:03 +0900

    certification25 재풀이

--- certification25/javascript/script.js
+++ certification25/javascript/script.js
@@ -1,21 +1,22 @@
 jQuery(document).ready(function() {
-  // 메뉴
-  $("#menu_bg").hide();
+// 메뉴
   $(".submenu").hide();
+  $("#bg_menu").hide();
   $(".navi>li").mouseover(function() {
     $(".submenu").stop().slideDown(500);
-    $("#menu_bg").stop().slideDown(500);
+    $("#bg_menu").stop().slideDown(500);
   }).mouseout(function() {
     $(".submenu").stop().slideUp(500);
-    $("#menu_bg").stop().slideUp(500);
+    $("#bg_menu").stop().slideUp(500);
   });
 
-  //이미지 슬라이드
+  // 이미지슬라이드
   $(".imgslide a:gt(0)").hide();
-  setInterval(function() {
-    $(".imgslide a:first-child").fadeOut().next("a").fadeIn().end().appendTo(".imgslide");
+  setInterval(() => {
+    $(".imgslide a:first-child").fadeOut().next("a").fadeIn().end().appendTo(".imgslide")
   }, 3000);
 
+  // 탭기능
   (function() {
     $(".tab>ul>li>a").click(function() {
       $(this).parent().addClass("active").siblings().removeClass("active");
@@ -23,10 +24,11 @@ jQuery(document).ready(function() {
     })
   })();
 
+  // 레이어 팝업
   $(".notice ul li:first-child").click(function() {
     $("#layer").addClass("active")
-  })
+  });
   $(".btn").click(function() {
     $("#layer").removeClass("active")
-  })
-})
+  });
+});
